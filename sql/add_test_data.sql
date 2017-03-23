034d311eea1452bab1bc317827d9970a548e390f
thureson <tom.melker@gmail.com>
2017-03-23 13:55:18 +0200

    PSQL support, sivuluonnokset lisätty

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,8 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Puolue (id, puolue, kuvaus) VALUES (1, 'Kokoomus', 'Suomen perinteinen oikeistopuolue.');
+INSERT INTO Puolue (id, puolue, kuvaus) VALUES (2, 'SDP', 'Suomen perinteinen vasemmistopuolue.');
+INSERT INTO Puolue (id, puolue, kuvaus) VALUES (3, 'Vihreät', 'Eritoten pääkaupunkiseudulla vaikuttava ympäristöpuolue.');
+INSERT INTO Puolue (id, puolue, kuvaus) VALUES (4, 'Piraattipuolue', 'Hiton käpistelijät.');
+INSERT INTO Kayttaja (id, tunnus) VALUES (1, 'Tom');
+INSERT INTO Ehdokas (id, nimi, kuvaus, puolue_id) VALUES (1, 'Sauli Niinistö', 'Presidentti', 1);
+INSERT INTO Historia (id, ehdokas_id, aanimaara, vuosi) VALUES (1, 1, 2000000, 2012);
+INSERT INTO Salasana (id, salasana, kayttaja_id) VALUES (1, 'test', 1);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,30 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Puolue (
+  id INTEGER PRIMARY KEY,
+  puolue varchar(50) NOT NULL,
+  kuvaus varchar(150)
+);
+
+CREATE TABLE Kayttaja (
+  id INTEGER PRIMARY KEY,
+  tunnus varchar(50) NOT NULL
+);
+
+CREATE TABLE Ehdokas (
+  id INTEGER PRIMARY KEY,
+  nimi varchar(50) NOT NULL,
+  kuvaus varchar(150) NOT NULL,
+  puolue_id INTEGER REFERENCES Puolue(id)
+);
+
+CREATE TABLE Historia (
+  id INTEGER PRIMARY KEY,
+  ehdokas_id INTEGER REFERENCES Ehdokas(id),
+  aanimaara INTEGER,
+  vuosi INTEGER
+);
+
+CREATE TABLE Salasana (
+  id INTEGER PRIMARY KEY,
+  salasana varchar(50) NOT NULL,
+  kayttaja_id INTEGER REFERENCES Kayttaja(id)
+);
